09d9212c8745bca98999bd75a63897d4121ca9bd
Vladimir Almaev <almaev.vladimir@gmail.com>
2016-02-21 18:28:45 +0300

    Added OwinHost project

---
+++ Source/DeepHeadz.Booking.Http.OwinHost/Properties/AssemblyInfo.fs
@@ -0,0 +1,7 @@
+﻿namespace DeepHeadz.Booking.Http.OwinHost
+open System.Reflection
+
+[<assembly: AssemblyTitle("DeepHeadz.Booking.Http.OwinHost")>]
+[<assembly: AssemblyCopyright("valmaev")>]
+[<assembly: AssemblyVersion("1.0.0.0")>]
+()
