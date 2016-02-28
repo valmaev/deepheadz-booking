f11007d97590f86506625f5b27e773f240246bb2
Vladimir Almaev <almaev.vladimir@gmail.com>
2016-02-28 20:31:08 +0300

    Added Fake build script

---
+++ build.fsx
@@ -0,0 +1,19 @@
+#I @"packages/FAKE/tools"
+#r @"packages/FAKE/tools/FakeLib.dll"
+
+open Fake
+
+let outputDir = "Artifacts"
+
+Target "Clean" (fun _ ->
+    CleanDir outputDir)
+
+Target "Build" (fun _ ->
+    !! "DeepHeadz.Booking.sln"
+    |> MSBuildRelease outputDir "Rebuild"
+    |> Log "Build Output: ")
+
+"Clean"
+    ==> "Build"
+
+RunTargetOrDefault "Build"
